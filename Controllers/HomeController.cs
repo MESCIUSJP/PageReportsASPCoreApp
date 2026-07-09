04989954befaae8bbdf3b8a7cd5199a7d2f880fd
Masataka Yonei <masataka.yonei@mescius.com>
2026-07-09 09:47:29 +0900

    初期コミット

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PageReportsASPCoreApp
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public object Index() => View("Index");
+    }
+}
